78dde3a52839bbd7fd48c1e05a2f2a6c62a712cc
developer <dka.tech.dev@gmail.com>
2024-06-24 01:22:17 +0800

    Add Node Support

--- dka-web-server/www/index.php
+++ dka-web-server/www/index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+$host = 'dka-mariadb'; // Docker service name
+$user = 'root'; // Database username
+$password = 'Cyberhack2010'; // Database password
+$database = 'mysql'; // Database name
+$port = 3306; // MariaDB port
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
+?>
