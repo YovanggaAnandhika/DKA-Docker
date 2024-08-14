10132f74cb041fe8a3637db41dff7636ba8e8e5b
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-08-14 23:48:42 +0800

    new update

--- dka-web-server/app/src/index.ts
+++ dka-web-server/app/src/index.ts
@@ -1,13 +1,57 @@
-// @ts-ignore
-import express, { Request, Response } from "express";
+import mqtt from "mqtt";
+// Ganti dengan nama service EMQX di docker-compose
+const brokerUrl = 'mqtt://localhost:1883';
+const clientId = 'nodejs_mqtt_client';
 
-const app = express()
-const port = 3000
 
-app.get('/', (req : Request, res : Response) => {
-    res.send('Hello World 2!')
+// Menghubungkan ke broker EMQX
+const client = mqtt.connect(brokerUrl, {
+    clientId: clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'developer', // Ganti dengan username yang telah ditambahkan
+    password: 'Cyberhack2010', // Ganti dengan password yang telah ditambahkan
+    reconnectPeriod: 1000,
 });
 
-app.listen(port, "0.0.0.0", () => {
-    console.log(`Example app listening on port ${port}`)
+client.on('connect', () => {
+    console.log('Connected to EMQX');
+
+    // Subscribe ke topik
+    const topic = 'test/topic';
+    client.subscribe(topic, (err) => {
+        if (!err) {
+            console.log(`Subscribed to ${topic}`);
+        }
+    });
+
+    client.publish(topic, Buffer.from('Hello MQTT'), { qos: 0, retain: false }, (err) => {
+        if (err) {
+            console.error('Failed to publish message:', err);
+        } else {
+            console.log('Message published');
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    console.log(`Received message: ${message.toString()} from topic: ${topic}`);
+});
+
+client.on('error', (err) => {
+    console.error('Connection error:', err);
+});
+
+client.on('reconnect', () => {
+    console.log('Reconnecting...');
+});
+
+client.on("disconnect",() => {
+
 })
+
+client.on('close', () => {
+    console.log('Connection closed');
+});
+
+
