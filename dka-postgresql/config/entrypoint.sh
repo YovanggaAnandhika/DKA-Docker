70f4911a6a7e1fa3e5523fe263ce8b6054f2ce07
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-04-02 01:46:27 +0800

    add stage

---
+++ dka-postgresql/config/entrypoint.sh
@@ -0,0 +1,181 @@
+#!/bin/sh
+
+HOSTNAME=$(hostname)
+DEFAULT_CONFIG_PATH=${DKA_CONFIG_PATH:-/var/lib/postgresql/data/postgresql.conf}
+DEFAULT_CONFIG_HBA_PATH=${DKA_CONFIG_HBA_PATH:-/var/lib/postgresql/data/pg_hba.conf}
+# Cron Env
+ENABLED_CRON=${DKA_CRON_ENABLE:-false}
+CRON_PRIODIC=${DKA_CRON_PRIODIC:-0 3 * * *}
+# Set default values if environment variables are not provided
+ROOT_PASSWORD=${DKA_ROOT_PASSWORD:-postgres}
+DB_NAME=${DKA_DB_NAME:-test}
+DB_USERNAME=${DKA_DB_USERNAME:-test}
+DB_PASSWORD=${DKA_DB_PASSWORD:-test}
+
+DB_MAX_CONNECTION=${DKA_DB_MAX_CONNECTION:-200}
+
+set_hba() {
+  # Mengizinkan koneksi dari semua host untuk semua database dan user menggunakan MD5
+  echo "host    all             all             0.0.0.0/0            scram-sha-256" >> $DEFAULT_CONFIG_HBA_PATH
+  echo "host    all             all             ::/0                 scram-sha-256" >> $DEFAULT_CONFIG_HBA_PATH
+}
+
+set_memory() {
+  # PERCENT_MEMORY sebagai persentase
+  PERCENT_MEMORY=0.8
+  # Membaca nilai memory.max dalam byte
+  MEMORY_MAX=$(cat /sys/fs/cgroup/memory.max 2>/dev/null)
+  # Cek jika nilai memory.max adalah "max" atau kosong
+  # shellcheck disable=SC3014
+  if [ "$MEMORY_MAX" == "max" ] || [ -z "$MEMORY_MAX" ]; then
+      echo "memory.max not exist. check with memory.limit_in_bytes ..."
+      MEMORY_MAX=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes 2>/dev/null)
+      # Jika memory.limit_in_bytes juga tidak ada, gunakan total memori host
+      if [ -z "$MEMORY_MAX" ]; then
+          echo "limit max memory not exist. fallback use host max memory"
+          MEMORY_MAX=$(free -b | grep Mem | awk '{print $2}')
+      fi
+  fi
+
+  # Mengalikan MEMORY_MAX dengan PERCENT_MEMORY menggunakan bc
+  MEMORY_MAX=$(echo "$MEMORY_MAX * $PERCENT_MEMORY" | bc)
+  # Membulatkan hasil ke bawah menjadi bilangan bulat
+  MEMORY_MAX=$(printf "%.0f" "$MEMORY_MAX")
+  # Mengonversi nilai byte ke MB
+  MEMORY_MAX_MB=$((MEMORY_MAX / 1024 / 1024))
+  # Menambahkan akhiran 'M' untuk format MB
+  MEMORY_MAX_MB="${MEMORY_MAX_MB}M"
+  echo "Limit memory is $MEMORY_MAX_MB. detected";
+
+  # Mengatur shared_buffers di PostgreSQL
+  SHARED_BUFFERS=$((MEMORY_MAX / 4 / 1024 / 1024))
+  SHARED_BUFFERS="${SHARED_BUFFERS}MB"
+
+  # Hitung work_mem dalam kB
+  WORK_MEM=$((MEMORY_MAX / 4 / DB_MAX_CONNECTION / 1024))
+  WORK_MEM="${WORK_MEM}kB"
+
+  # Mengganti shared_buffers dengan nilai dari variabel SHARED_BUFFERS
+  sed -i "s|^\s*shared_buffers = 128MB|shared_buffers = $SHARED_BUFFERS|g" $DEFAULT_CONFIG_PATH
+  # Mengganti max_connection dengan nilai dari variabel $DB_MAX_CONNECTION
+  sed -i "s|^\s*max_connections = 100|max_connections = $DB_MAX_CONNECTION|g" $DEFAULT_CONFIG_PATH
+  # Ganti work_mem
+  sed -i "s|^\s*#*work_mem\s*=.*|work_mem = $WORK_MEM|g" $DEFAULT_CONFIG_PATH
+  # Menghapus komentar dari password_encryption
+  sed -i "s|^\s*#*password_encryption = scram-sha-256|password_encryption = scram-sha-256|g" $DEFAULT_CONFIG_PATH
+  # Mengganti listen_addresses agar PostgreSQL mendengarkan koneksi dari semua alamat IP
+  sed -i "s|^\s*#*listen_addresses = 'localhost'|listen_addresses = '*'|g" $DEFAULT_CONFIG_PATH
+
+}
+
+checkPostgreSQLIsRunning(){
+  # Wait for PostgreSQL to start
+    until pg_isready >/dev/null 2>&1; do
+        echo "Waiting for PostgreSQL to start..."
+        sleep 2
+    done
+    echo "PostgreSQL Server Is Running..."
+}
+
+# Fungsi untuk memulai PostgreSQL
+initiate_postgresql() {
+  echo "Starting PostgreSQL Temporary..."
+  pg_ctl start -D /var/lib/postgresql/data &
+  pid="$!"
+  checkPostgreSQLIsRunning
+}
+
+set_users_and_grant() {
+  # Mengatur user dan grant
+  psql -U postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD '$ROOT_PASSWORD';"
+  psql -U postgres -c "CREATE USER $DB_USERNAME WITH ENCRYPTED PASSWORD '$DB_PASSWORD';"
+  psql -U postgres -c "CREATE DATABASE $DB_NAME;"
+  psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
+}
+
+load_init_sql_template() {
+  # Mengeksekusi skrip SQL dari direktori /docker-entrypoint-initdb.d jika ada
+  if [ -d "/docker-entrypoint-initdb.d" ]; then
+      for sql_file in /docker-entrypoint-initdb.d/*.sql; do
+          if [ -f "$sql_file" ]; then
+              echo "Running script : $sql_file..."
+              psql -U postgres -f "$sql_file"
+          fi
+      done
+  fi
+}
+
+load_automation_sql_template() {
+  echo "checking sql file script on /docker-entrypoint.d if exist"
+  # Mengeksekusi skrip SQL dari direktori /docker-entrypoint.d jika ada
+  if [ -d "/docker-entrypoint.d" ]; then
+      for sql_file in /docker-entrypoint.d/*.sql; do
+          if [ -f "$sql_file" ]; then
+              echo "Running script : $sql_file..."
+              psql -U postgres -f "$sql_file"
+          fi
+      done
+  fi
+  echo "task sql file automation is complete"
+}
+
+load_cron_scheduler(){
+  # Menjalankan cron jika diaktifkan
+  if [ "$ENABLED_CRON" = "true" ]; then
+    for file in /usr/cron.d/*; do
+      if [ -x "$file" ]; then
+        cron_name=$(basename "$file")
+        # Menambahkan log output ke file log
+        echo "$CRON_PRIODIC /bin/bash $file >> /var/log/postgresql/cron.log 2>&1" > "/etc/cron.d/$cron_name"
+      fi
+    done
+  fi
+}
+
+checkIsInitDB(){
+  # Memeriksa apakah database sudah ada
+  if [ ! -d "/var/lib/postgresql/data/PG_VERSION" ]; then
+      echo "first Run. initiate system server..."
+      pg_ctl init -D /var/lib/postgresql/data
+      echo "Database Successfully initiate..."
+      initiate_postgresql
+      set_users_and_grant
+      load_init_sql_template
+      echo "shutdown PostgreSQL Temporary..."
+      pg_ctl stop -D /var/lib/postgresql/data
+      wait "$pid"
+      echo "PostgreSQL successfully stopped."
+      set_memory
+      set_hba
+  else
+      echo "system PostgreSQL is initiate.., continue running server."
+  fi
+}
+
+# Fungsi untuk memonitor PostgreSQL dan restart jika gagal
+watch_services() {
+  echo "started health monitoring process ..."
+  while true; do
+    # Memeriksa apakah postgresql dan cron berjalan
+    if ! pgrep postgres > /dev/null; then
+      echo "one or more process stopped. exit container ..."
+      exit 1
+    fi
+    sleep 3
+  done
+}
+
+echo "checking init server..."
+checkIsInitDB
+echo "load cron scheduler..."
+load_cron_scheduler
+echo "Running Logrotate..."
+logrotate -f /etc/logrotate.conf >/dev/null 2>&1;
+echo "Running Scheduler Cron"
+crond &
+# Memulai server PostgreSQL secara normal
+echo "Final Running PostgreSQL..."
+pg_ctl start -D /var/lib/postgresql/data &
+checkPostgreSQLIsRunning
+load_automation_sql_template
+watch_services
