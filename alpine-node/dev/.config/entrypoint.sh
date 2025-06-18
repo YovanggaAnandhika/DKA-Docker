b9a9b7e491435e6b9d0f66e69574a5629ebbf0f4
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-06-19 01:46:05 +0800

    add stage

---
+++ alpine-node/dev/.config/entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+set -e
+
+TUNNEL_NAME=${DKA_TUNNEL_NAME:-$(hostname)}
+
+createDNSRoute() {
+  echo "🔍 Checking is DNS Is Exist for $TUNNEL_NAME..."
+  # Cek dari cloudflared tunnel route list (DNS)
+  if cloudflared tunnel route dns --list 2>/dev/null | grep -qw "$TUNNEL_NAME"; then
+    echo "✅ DNS route for $TUNNEL_NAME exists, skip registration DNS route."
+  else
+    echo "🌐 Adding DNS route for $TUNNEL_NAME to $TUNNEL_ID..."
+    cloudflared tunnel route dns "$TUNNEL_ID" "$TUNNEL_NAME"
+    echo "✅ DNS route successfully added."
+  fi
+}
+
+createConfig() {
+  CONFIG_PATH="/root/.cloudflared/config.yml"
+  echo "🛠️ Make config.yml in $CONFIG_PATH"
+  cat <<-EOF > "$CONFIG_PATH"
+  tunnel: $TUNNEL_ID
+  credentials-file: $TUNNEL_JSON
+
+  ingress:
+    - hostname: ${TUNNEL_NAME:-example.com}
+      service: http://localhost:80
+    - service: http_status:404
+EOF
+  echo "✅ successfully created $CONFIG_PATH"
+}
+
+createTunnel() {
+ if cloudflared tunnel list | grep -qw "$TUNNEL_NAME"; then
+     echo "✅ Tunnel '$TUNNEL_NAME' is Exists, ignore recreate tunnel."
+     TUNNEL_ID=$(cloudflared tunnel list --output json | grep -B 2 "\"name\": \"$TUNNEL_NAME\"" | grep '"id":' | cut -d'"' -f4)
+     TUNNEL_JSON="/root/.cloudflared/$TUNNEL_ID.json"
+   else
+     echo "🚧 Tunnel '$TUNNEL_NAME' not exists, new create..."
+     TUNNEL_CREATE_OUTPUT=$(cloudflared tunnel create "$TUNNEL_NAME")
+     TUNNEL_JSON=$(echo "$TUNNEL_CREATE_OUTPUT" | grep -o '/root/.cloudflared/[^ ]*\.json')
+     TUNNEL_ID=$(grep -o '"TunnelID"[ ]*:[ ]*"[^"]*"' "$TUNNEL_JSON" | cut -d'"' -f4)
+   fi
+
+   echo "🔑 TUNNEL_ID: $TUNNEL_ID"
+   echo "📄 TUNNEL_JSON: $TUNNEL_JSON"
+}
+
+runningTunnel() {
+  cloudflared tunnel run "$TUNNEL_NAME" &
+}
+
+checkTunnelIsExist() {
+  if [ -f "/root/.cloudflared/cert.pem" ]; then
+    echo "Detected cloudflared configuration ..."
+    createTunnel
+    createConfig
+    createDNSRoute
+    runningTunnel
+  else
+    echo "🚧 not detected cloudflare tunnel configuration. skipped ..."
+    echo "🚧 to used tunnel. running in exec -it with 'cloudflared tunnel login' and set hostname in service compose.yml file"
+    echo "🚧 and create volume persistent to /root/.cloudflared to persistent volume. prevent loss cloudflared configuration"
+  fi
+}
+
+# Tunggu jika tidak ada argumen, atau eksekusi argumen jika ada
+if [ "$#" -gt 0 ]; then
+    checkTunnelIsExist
+    # If arguments exist, execute them
+    exec "$@"
+else
+    # If no arguments were passed, check if package.json exists
+    if [ -f "package.json" ]; then
+        echo "Starting default entry point main in package.json"
+        checkTunnelIsExist
+        node .
+    else
+        # If package.json doesn't exist, wait for background processes
+        echo "package.json not found. recreated container with command. or create main point field in package.json file"
+        tail -f /dev/null
+    fi
+fi
--- alpine-node/minimal/.config/entrypoint.sh
+++ alpine-node/minimal/.config/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-
 # Tunggu jika tidak ada argumen, atau eksekusi argumen jika ada
 if [ "$#" -gt 0 ]; then
     # If arguments exist, execute them
@@ -9,11 +8,11 @@ if [ "$#" -gt 0 ]; then
 else
     # If no arguments were passed, check if package.json exists
     if [ -f "package.json" ]; then
-        echo "Memulai Default Entry point main di package.json"
+        echo "Starting default entry point main in package.json"
         node .
     else
         # If package.json doesn't exist, wait for background processes
-        echo "package.json tidak ditemukan. Mulai Ulang Container dengan command. atau buat point di file package.json"
+        echo "package.json not found. recreated container with command. or create main point field in package.json file"
         tail -f /dev/null
     fi
 fi
